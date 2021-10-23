ee9895c8c0d9101082bd108a6958b88dbaa5cf78
TranHuyWin <tranhuywin11b1@gmail.com>
2021-10-23 09:42:07 +0700

    setup route, add image item introduce, add app scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import Login from './pages/login';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { LoginRoute } from './routers';
+import HeaderBrand from './components/headers/headerBrand';
+import FooterCertification from './components/footers/footerCertification';
 
+import './app.scss'
 function App() {
   return (
     <Router>
-      <Login />
+      <header>
+        <HeaderBrand />
+      </header>
+      
+      <Switch>
+        <Route path="/login">
+          <LoginRoute />
+        </Route>
+      </Switch>
+
+      <footer>
+        <FooterCertification />
+      </footer>
     </Router>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,5 @@
-import HeaderBrand from '../components/headers/headerBrand';
 import BannerSignUp from '../components/banner/bannerSignUp';
 import ItemIndtroduce from '../components/ItemIntroduce/itemIndtroduce';
-import FooterCertification from '../components/footers/footerCertification';
 
 function Login() {
   const styles = {
@@ -13,42 +11,40 @@ function Login() {
   }
   return (
     <>
-      <HeaderBrand />
       <BannerSignUp />
 
       <div style={styles.groupItem}>
         <div style={{ display: 'inline-block' }}>
-          <ItemIndtroduce image='/coins@2x.png' title='Tich diem nhanh chong'>
-            <span>Phương thức thanh toán tiện lợi, an toàn.
-              Tích hợp chức năng lưu thẻ để đặt phòng lần sau.</span>
+          <ItemIndtroduce image='/coins@2x.png' title='Tích điểm nhanh chóng'>
+            <span>Tích điểm đối với mỗi lượt đặt chỗ thành công.</span><br/>
+            <span>Quy đổi thành Lux Credit để du lịch nhiều hơn nữa.</span>
           </ItemIndtroduce>
         </div>
 
 
         <div style={{ marginLeft: '60px', display: 'inline-block', }}>
-          <ItemIndtroduce image='/coins@2x.png' title='Tich diem nhanh chong'>
-            <span>Phương thức thanh toán tiện lợi, an toàn.
-              Tích hợp chức năng lưu thẻ để đặt phòng lần sau.</span>
+          <ItemIndtroduce image='/wallet@2x.png' title='Thanh toán đơn giản'>
+            <span>Phương thức thanh toán tiện lợi, an toàn.</span><br/>
+            <span>Tích hợp chức năng lưu thẻ để đặt phòng lần sau.</span>
           </ItemIndtroduce>
         </div>
 
         <div style={{ display: 'inline-block', marginTop: '64px' }}>
-          <ItemIndtroduce image='/coins@2x.png' title='Tich diem nhanh chong'>
-            <span>Phương thức thanh toán tiện lợi, an toàn.
-              Tích hợp chức năng lưu thẻ để đặt phòng lần sau.</span>
+          <ItemIndtroduce image='/backpack@2x.png' title='Ưu đãi mỗi ngày'>
+            <span>Nhận thông báo ưu đãi từ Luxstay khi có kế hoạch
+              du lịch để lựa chọn và đặt ngay cho mình
+              một chỗ ở phù hợp, tiện nghi với giá tốt nhất.</span>
           </ItemIndtroduce>
         </div>
 
         <div style={{ marginLeft: '60px', display: 'inline-block', marginTop: '64px' }}>
-          <ItemIndtroduce image='/coins@2x.png' title='Tich diem nhanh chong'>
-            <span>Phương thức thanh toán tiện lợi, an toàn.
-              Tích hợp chức năng lưu thẻ để đặt phòng lần sau.</span>
+          <ItemIndtroduce image='/top-sales@2x.png' title='Tiện ích thông minh'>
+            <span>Check-in và kiểm tra hóa đơn thanh toán kể cả khi
+              không có kết nối mạng. Hoàn tiền nhanh gọn. </span><br/>
+            <span>Đổi lịch dễ dàng.</span>
           </ItemIndtroduce>
         </div>
       </div>
-
-      <FooterCertification />
-
     </>
   );
 }
